b50949bfda026b44fec2d0e414d23443ca2f7106
Harvey Unsaac <193837@unsaac.edu.pe>
2022-12-22 20:11:40 +0100

    :sparkles: Add simple views and principles routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
+* Route::get        Consultar
+* Route::post       Crear
+* Route::put        Actualizar
+* Route::delete     Eliminar
+* Route::post       Guardar
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Ruta para la vista de home
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+// Ruta para la vista de practicas
+Route::get('practices', function () {
+    return view('practices');
+});
+
+// Ruta para detalle de practica de practica
+Route::get('practices/{id}', function ($id) {
+    return view('practice', ['id' => $id]);
+});
+
+
+// Ruta para la vista de about
+
+// Ruta para la vista de contact
+
