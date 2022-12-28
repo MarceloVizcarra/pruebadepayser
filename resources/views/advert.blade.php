f15bd4ca6d64e8ddbf4e753eb3efc82d80dbad46
Harvey Unsaac <193837@unsaac.edu.pe>
2022-12-28 05:39:05 +0100

    :alien: Modify advert view

--- resources/views/advert.blade.php
+++ resources/views/advert.blade.php
@@ -1,5 +1,5 @@
 @php
-    $class = 'uppercase text-gray-700 rounded-full px-2 py-1';
+    $class = 'uppercase text-gray-900 font-medium rounded-full px-2 py-1.5';
     $Otros = $class . ' bg-orange-200';
     $Backend = $class . ' bg-blue-200';
     $Fullstack = $class . ' bg-red-200';
@@ -7,7 +7,7 @@
 @endphp
 
 <x-app-layout>
-
+    <!--  -->
     <form action="{{ route('advert') }}" method="GET" class="flex items-center justify-between mx-6 my-6">
         <input type="text" name="search" placeholder="Buscar" class="flex-grow border border-gray-300 rounded-full px-4 py-2">
         <select name="filter" id="filter" class="mx-4 border border-gray-300 rounded">
@@ -19,21 +19,23 @@
         </select>
         <input type="submit" value="Buscar" class="bg-gray-800 text-white rounded px-4 py-2">
     </form>
-    <div class="grid grid-cols-1 gap-4 mb-4">
-        @foreach ($employes as $employe)
-            <div class="bg-gray-100 rounded-lg px-6 py-4">
-                <p class="text-xs flex items-center gap-2 justify-between">
-                    <span class="{{ ${$employe->category} }}">{{ $employe->category }}</span>
-                    <span>{{ $employe -> created_at->format('d/m/Y') }}</span>
-                </p>
 
-                <h2>{{ $employe->title }}</h2>
-                <p>
-                    {{ $employe->content }}
-                </p>
-                <span> {{ $employe->user->name }}</span>
-                <a href="{{ route('employe', $employe->slug) }}">Ver</a><br><br>
-            </div>
+    <div class="grid grid-cols-1 gap-2 mb-4">
+        @foreach ($employes as $employe)
+            <a href="{{ route('employe', $employe->slug) }}" class="group bg-gray-100 hover:bg-gray-200 rounded-lg px-6 py-4 flex " >
+                <img src="{{ $employe->image }}" alt="logo" class="h-12 gap-4 mr-4 mt-4">
+                <div class="w-full grid grid-cols-1 gap-1">
+                    <h2 class="uppercase font-bold mt-2 text-base group-hover:underline">{{ $employe->title }}</h2>
+                    <span class=" font-semibold"> {{ $employe->company }}</span>
+                    <span class=" font-semibold text-gray-600 text-sm"> {{ $employe->location . ', ' . $employe->city . ', ' . $employe->country }}</span>
+                    <span class=" text-gray-600 text-sm"> Publicado: {{ $employe -> updated_at->format('d/m/Y') }}</span>
+                    <span class=" text-gray-600 text-sm"> Finaliza: {{ $employe->deadline }}</span>
+                    <p class="text-xs flex items-center justify-between">
+                        <span class="text-sm text-gray-600">{{ $employe->type }}</span>
+                        <span class="{{ ${$employe->category} }}">{{ $employe->category }}</span>
+                    </p>
+                </div>
+            </a>
         @endforeach
     </div>
 
